861843022d31acd1385e2cbdc1c49241485941f9
Ana Jurado <ana.juradol@uniagustiniana.edu.co>
2026-05-27 15:33:05 -0500

    Add transforms module and basic validation tests

---
+++ src/melfa_kinematics/transforms.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+def Rx(theta):
+    c = np.cos(theta)
+    s = np.sin(theta)
+
+    return np.array([
+        [1, 0, 0, 0],
+        [0, c, -s, 0],
+        [0, s, c, 0],
+        [0, 0, 0, 1]
+    ])
+
+
+def Ry(theta):
+    c = np.cos(theta)
+    s = np.sin(theta)
+
+    return np.array([
+        [c, 0, s, 0],
+        [0, 1, 0, 0],
+        [-s, 0, c, 0],
+        [0, 0, 0, 1]
+    ])
+
+
+def Rz(theta):
+    c = np.cos(theta)
+    s = np.sin(theta)
+
+    return np.array([
+        [c, -s, 0, 0],
+        [s, c, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+
+def Trans(x, y, z):
+    return np.array([
+        [1, 0, 0, x],
+        [0, 1, 0, y],
+        [0, 0, 1, z],
+        [0, 0, 0, 1]
+    ])
