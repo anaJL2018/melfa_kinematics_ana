a33653610087b7245ecf36e2dcd0d5995be37bbb
Ana Jurado <ana.juradol@uniagustiniana.edu.co>
2026-05-27 19:29:12 -0500

    Add homogeneous rotation transformations

--- src/melfa_kinematics/transforms.py
+++ src/melfa_kinematics/transforms.py
@@ -1,46 +1,45 @@
 import numpy as np
 
 
-def Rx(theta):
-    c = np.cos(theta)
-    s = np.sin(theta)
-
-    return np.array([
-        [1, 0, 0, 0],
-        [0, c, -s, 0],
-        [0, s, c, 0],
-        [0, 0, 0, 1]
-    ])
-
-
-def Ry(theta):
-    c = np.cos(theta)
-    s = np.sin(theta)
-
-    return np.array([
-        [c, 0, s, 0],
-        [0, 1, 0, 0],
-        [-s, 0, c, 0],
-        [0, 0, 0, 1]
-    ])
-
-
-def Rz(theta):
-    c = np.cos(theta)
-    s = np.sin(theta)
-
-    return np.array([
-        [c, -s, 0, 0],
-        [s, c, 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]
-    ])
-
-
 def Trans(x, y, z):
-    return np.array([
+    T = np.array([
         [1, 0, 0, x],
         [0, 1, 0, y],
         [0, 0, 1, z],
         [0, 0, 0, 1]
     ])
+
+    return T
+
+
+def RotX(theta):
+    R = np.array([
+        [1, 0, 0, 0],
+        [0, np.cos(theta), -np.sin(theta), 0],
+        [0, np.sin(theta), np.cos(theta), 0],
+        [0, 0, 0, 1]
+    ])
+
+    return R
+
+
+def RotY(theta):
+    R = np.array([
+        [np.cos(theta), 0, np.sin(theta), 0],
+        [0, 1, 0, 0],
+        [-np.sin(theta), 0, np.cos(theta), 0],
+        [0, 0, 0, 1]
+    ])
+
+    return R
+
+
+def RotZ(theta):
+    R = np.array([
+        [np.cos(theta), -np.sin(theta), 0, 0],
+        [np.sin(theta), np.cos(theta), 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+
+    return R
